b8b6eebc7b3cae2df0a6ce31fc9e44fc2b54443f
Rhadamez Gindri Hercilio <rhadamez@gmail.com>
2022-01-01 18:29:42 -0300

    move business logic to a domain entity

---
+++ tests/domain/models/facebook-account.spec.ts
@@ -0,0 +1,49 @@
+import { FacebookAccount } from '@/domain/models/facebook-account'
+
+describe('FacebookAccount', () => {
+  const fbData = {
+    name: 'any_fb_name',
+    email: 'any_fb_email',
+    facebookId: 'any_fb_id'
+  }
+
+  it('should create with facebook data only', () => {
+    const sut = new FacebookAccount(fbData)
+
+    expect(sut).toEqual({
+      name: 'any_fb_name',
+      email: 'any_fb_email',
+      facebookId: 'any_fb_id'
+    })
+  })
+
+  it('should update name if it is empty', () => {
+    const accountData = {
+      id: 'any_id'
+    }
+    const sut = new FacebookAccount(fbData, accountData)
+
+    expect(sut).toEqual({
+      id: 'any_id',
+      name: 'any_fb_name',
+      email: 'any_fb_email',
+      facebookId: 'any_fb_id'
+    })
+  })
+
+  it('should not update name if it is filled', () => {
+    const accountData = {
+      id: 'any_id',
+      name: 'any_name'
+    }
+
+    const sut = new FacebookAccount(fbData, accountData)
+
+    expect(sut).toEqual({
+      id: 'any_id',
+      name: 'any_name',
+      email: 'any_fb_email',
+      facebookId: 'any_fb_id'
+    })
+  })
+})
