462073a14306a2877e98214373696bf0927759cb
Rhadamez Gindri Hercilio <rhadamez@gmail.com>
2022-03-17 01:45:08 -0300

    replace integration tests in favor of mocking

--- src/domain/features/facebook-authentication.ts
+++ src/domain/features/facebook-authentication.ts
@@ -2,7 +2,7 @@ import { AccessToken } from '@/domain/models'
 import { AuthenticationError } from '@/domain/errors'
 
 export interface FacebookAuthentication {
-  perform (params: FacebookAuthentication.Params): Promise<FacebookAuthentication.Result>
+  perform(params: FacebookAuthentication.Params): Promise<FacebookAuthentication.Result>
 }
 
 export namespace FacebookAuthentication {
