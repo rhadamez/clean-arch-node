d163353d3bc7866bacbd06f342fcb906c4c23a0b
Rhadamez Gindri Hercilio <rhadamez@gmail.com>
2022-09-21 00:34:10 -0300

    move production code to their own files

--- tests/application/controllers/facebook-login.spec.ts
+++ tests/application/controllers/facebook-login.spec.ts
@@ -1,57 +1,10 @@
+import { mock, MockProxy } from 'jest-mock-extended'
+
 import { AuthenticationError } from '@/domain/errors'
 import { FacebookAuthentication } from '@/domain/features'
 import { AccessToken } from '@/domain/models'
-import { mock, MockProxy } from 'jest-mock-extended'
-import { LockNotSupportedOnGivenDriverError } from 'typeorm'
-
-class FacebookLoginController {
-  constructor(private readonly facebookAuthentication: FacebookAuthentication) {}
-
-  async handle(httpRequest: any): Promise<HttpResponse> {
-    try {
-      if (httpRequest.token === '' || httpRequest.token === null || httpRequest.token === undefined) {
-        return {
-          statusCode: 400,
-          data: new Error('The field token is required')
-        }
-      }
-
-      const result = await this.facebookAuthentication.perform({ token: httpRequest.token })
-
-      if (result instanceof AccessToken) {
-        return {
-          statusCode: 200,
-          data: {
-            accessToken: result.value
-          }
-        }
-      } else {
-        return {
-          statusCode: 401,
-          data: result
-        }
-      }
-    } catch (error: any) {
-      return {
-        statusCode: 500,
-        data: new ServerError(error)
-      }
-    }
-  }
-}
-
-type HttpResponse = {
-  statusCode: number
-  data: any
-}
-
-class ServerError extends Error {
-  constructor(error?: Error) {
-    super('Server failed. Try again soon')
-    this.name = 'ServerError'
-    this.stack = error?.stack
-  }
-}
+import { ServerError } from '@/application/errors'
+import { FacebookLoginController } from '@/application/controllers/facebook-login'
 
 describe('FacebookLoginController', () => {
   let sut: FacebookLoginController
