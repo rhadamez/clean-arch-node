b787489c3f0ee062c5f015921c20b18f1cb209a3
Rhadamez Gindri Hercilio <rhadamez@gmail.com>
2022-11-08 00:58:23 -0300

    move RequiredStringValidator to its own file

--- src/application/controllers/facebook-login.ts
+++ src/application/controllers/facebook-login.ts
@@ -1,7 +1,7 @@
 import { FacebookAuthentication } from '@/domain/features'
 import { AccessToken } from '@/domain/models'
-import { RequiredFieldError, ServerError } from '../errors/http'
 import { badRequest, HttpResponse, ok, serverError, unauthorized } from '../helpers'
+import { RequiredStringValidator } from '../validation'
 
 type HttpRequest = {
   token: string
@@ -31,8 +31,7 @@ export class FacebookLoginController {
   }
 
   private validate(httpRequest: HttpRequest): Error | undefined {
-    if (httpRequest.token === '' || httpRequest.token === null || httpRequest.token === undefined) {
-      return new RequiredFieldError('token')
-    }
+    const validator = new RequiredStringValidator(httpRequest.token, 'token')
+    return validator.validate()
   }
 }
